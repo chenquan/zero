b46012681c6d381e51db7113965099e9e2fa2881
Chen Quan <chenquan@osai.club>
2020-10-03 22:53:36 +0800

    Add 分组/中间件/HTML模板/错误恢复 (#1)

--- zero.go
+++ zero.go
@@ -18,7 +18,10 @@
 package zero
 
 import (
+	"html/template"
 	"net/http"
+	"path"
+	"strings"
 )
 
 // HandlerFunc defines the request handler used by gee
@@ -26,34 +29,143 @@ type HandlerFunc func(*Context)
 
 // Engine implement the interface of ServeHTTP
 type Engine struct {
-	router *router
+	*RouterGroup
+	router        *router
+	groups        []*RouterGroup     // 存储所有group
+	htmlTemplates *template.Template // for html render
+	funcMap       template.FuncMap   // for html render
 }
 
 func (e *Engine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	e.router.handle(newContext(writer, request))
+	var middlewares []HandlerFunc
+	// 获取分组中所有中间件
+	for _, group := range e.groups {
+		if strings.HasPrefix(request.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+	c := newContext(writer, request)
+	c.handlers = middlewares
+	c.engine = e
+	e.router.handle(c)
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+func NewDefault() *Engine {
+	engine := New()
+	engine.Use(LoggerDefault(), Recovery())
+	return engine
 }
 
-func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	e.router.addRoute(method, pattern, handler)
-}
-func (e *Engine) GET(pattern string, handler HandlerFunc) {
-	e.addRoute("GET", pattern, handler)
-}
-func (e *Engine) POST(pattern string, handler HandlerFunc) {
-	e.addRoute("POST", pattern, handler)
-}
-func (e *Engine) PUT(pattern string, handler HandlerFunc) {
-	e.addRoute("PUT", pattern, handler)
-}
-func (e *Engine) DELETE(pattern string, handler HandlerFunc) {
-	e.addRoute("DELETE", pattern, handler)
+func (e *Engine) SetFuncMap(funcMap template.FuncMap) {
+	e.funcMap = funcMap
 }
 
-// Run defines the method to start a http server
+func (e *Engine) LoadHTMLGlob(pattern string) {
+	e.htmlTemplates = template.Must(template.New("").Funcs(e.funcMap).ParseGlob(pattern))
+}
+func (e *Engine) addRoute(method string, pattern string, handlers ...HandlerFunc) {
+	e.router.addRoute(method, pattern, handlers...)
+}
+func (e *Engine) GET(pattern string, handlers ...HandlerFunc) {
+	e.addRoute(GET, pattern, handlers...)
+}
+func (e *Engine) POST(pattern string, handlers ...HandlerFunc) {
+	e.addRoute(POST, pattern, handlers...)
+}
+func (e *Engine) PUT(pattern string, handlers ...HandlerFunc) {
+	e.addRoute(PUT, pattern, handlers...)
+}
+func (e *Engine) DELETE(pattern string, handlers ...HandlerFunc) {
+	e.addRoute(DELETE, pattern, handlers...)
+}
+func (e *Engine) HEAD(pattern string, handlers ...HandlerFunc) {
+	e.addRoute(HEAD, pattern, handlers...)
+}
+
+// Run 启动http服务器
 func (e *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, e)
+}
+
+// 分组路由
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // 中间件
+	parent      *RouterGroup  // 支持嵌套
+	engine      *Engine       // 所有组共享一个Engine实例
+}
+
+func (rg *RouterGroup) Group(prefix string) *RouterGroup {
+
+	newGroup := &RouterGroup{
+		prefix: rg.prefix + prefix,
+		parent: rg,
+		engine: rg.engine,
+	}
+	rg.engine.groups = append(rg.engine.groups, newGroup)
+	return newGroup
+}
+
+func (rg *RouterGroup) Use(middlewares ...HandlerFunc) {
+	rg.middlewares = append(rg.middlewares, middlewares...)
+}
+func (rg *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(rg.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		// Check if file exists and/or if we have permission to access it
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		fileServer.ServeHTTP(c.Res, c.Req)
+	}
+}
+
+// serve static files
+func (rg *RouterGroup) Static(relativePath string, root string) {
+	handler := rg.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	// Register GET handlers
+	rg.GET(urlPattern, handler)
+}
+
+// addRoute 添加路由
+func (rg *RouterGroup) addRoute(method string, prefix string, handlers ...HandlerFunc) {
+	// 真实路径 = 分组路径+当前路径
+	pattern := rg.prefix + prefix
+	rg.engine.addRoute(method, pattern, handlers...)
+}
+
+// GET 添加GET路由
+func (rg *RouterGroup) GET(pattern string, handlers ...HandlerFunc) {
+	rg.addRoute(GET, pattern, handlers...)
+}
+
+// POST 添加POST路由
+func (rg *RouterGroup) POST(pattern string, handlers ...HandlerFunc) {
+	rg.addRoute(POST, pattern, handlers...)
+}
+
+// PUT 添加PUT路由
+func (rg *RouterGroup) PUT(pattern string, handlers ...HandlerFunc) {
+	rg.addRoute(PUT, pattern, handlers...)
+}
+
+// DELETE 添加DELETE路由
+func (rg *RouterGroup) DELETE(pattern string, handlers ...HandlerFunc) {
+	rg.addRoute(POST, pattern, handlers...)
+}
+
+// HEAD 添加HEAD路由
+func (rg *RouterGroup) HEAD(pattern string, handlers ...HandlerFunc) {
+	rg.addRoute(HEAD, pattern, handlers...)
 }
